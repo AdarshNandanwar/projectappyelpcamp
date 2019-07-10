be4a2ba78fa527284c8f5c3d4be66fc9271d9416
AdarshNandanwar <f20180396@goa.bits-pilani.ac.in>
2019-07-10 23:31:27 +0530

    add mongoDB Atlas

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://user1:pass1@cluster0-imtey.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("ERROR: ", err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));     //Adding custom CSS
